4a0468120eeec30a36d024c3ed8d9a2922ffe454
2horse9sun <jsjhfx@163.com>
2020-07-10 07:49:06 +0800

    stage 4 finished

--- simulator/dv_router.py
+++ simulator/dv_router.py
@@ -130,6 +130,14 @@ class DVRouter(DVRouterBase):
         :return: nothing.
         """
         # TODO: fill this in!
+        new_latency = route_latency + self.ports.get_latency(port)
+        if route_dst not in self.table.keys():
+            self.table[route_dst] = TableEntry(route_dst, port, new_latency, api.current_time() + self.ROUTE_TTL)
+        else:
+            current_latency = self.table[route_dst][2]
+            current_port = self.table[route_dst][1]
+            if new_latency < current_latency or port == current_port:
+                self.table[route_dst] = TableEntry(route_dst, port, new_latency, api.current_time() + self.ROUTE_TTL)
 
     def handle_link_up(self, port, latency):
         """
